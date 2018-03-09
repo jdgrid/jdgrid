9ed827c6dbc2f4bf818e88158338ba973ef8cf22
f41s4n <f41s4n@gmail.com>
2018-03-09 12:42:11 +0100

    Creada la nueva oestructura del proyecto

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,10 @@ const gulp = require('gulp'),
 
 // Opciones de configuración
 
+// Array que contiene los componentes de la interfaz de usuario
+// Este array se utiliza para recorrer la estructura de carpetas,
+// cada componente. Así compilamos todos los componentes que
+// necesitemos en cada momento.
 let components = [
     'tooltip'
 ];
@@ -15,15 +19,31 @@ let sassOptions = {
     outputStyle: 'compressed'
 };
 
+// Opciones para el autoprefijado
 let autoprefixerOptions = {
     versions: '4 last browsers'
 };
 
+// Opciones para pug
+let pugOptions = {
+    pretty: true
+};
+
+
 // Componentes de la interfaz de usuario
-gulp.task('sass-components', () =>{
+
+// SASS
+gulp.task('sass-ui', () =>{
     for (let component of components)
-        gulp.src(`./dev/scss/components/${component}/default.scss`)
+        gulp.src(`./dev/ui/scss/components/${component}/default.scss`)
             .pipe(sass(sassOptions))
             .pipe(autoprefixer(autoprefixerOptions))
-            .pipe(gulp.dest(`./dist/css/${component}`));
+            .pipe(gulp.dest(`./dist/uicss/${component}`));
+});
+
+// PUG
+gulp.task('pug-ui', () =>{
+    gulp.src('./dev/ui/pug/*.pug')
+        .pipe(pug(pugOptions))
+        .pipe(gulp.dest('./dist/ui'));
 });
